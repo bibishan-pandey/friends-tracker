859afef0594c95da4b85f302c87a34941d55df65
bivishanpandey <natsu.dragneel.bivishan@example.com>
2018-08-12 09:59:44 +0545

    Initial commit

---
+++ app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/SigninActivity.java
@@ -0,0 +1,14 @@
+package com.project.natsu_dragneel.people_tracker_android_java;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SigninActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+}
