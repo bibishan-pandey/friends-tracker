1cb94d73b26a4af753494f56929e5c2905d86e88
bivishanpandey <natsu.dragneel.bivishan@example.com>
2018-08-14 15:51:37 +0545

    icon commit

--- app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/UserLocationMainActivity.java
+++ app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/UserLocationMainActivity.java
@@ -3,23 +3,20 @@ package com.project.natsu_dragneel.people_tracker_android_java;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -81,8 +78,8 @@ public class UserLocationMainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_location_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         auth = FirebaseAuth.getInstance();
         user=auth.getCurrentUser();
@@ -92,10 +89,10 @@ public class UserLocationMainActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                //this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawer.addDrawerListener(toggle);
+        //toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -121,6 +118,15 @@ public class UserLocationMainActivity extends AppCompatActivity
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
@@ -248,7 +254,7 @@ public class UserLocationMainActivity extends AppCompatActivity
 
             MarkerOptions options=new MarkerOptions();
             //mMap.clear();
-            options.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_location));
+            options.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_location_on));
             options.position(latLng);
             options.title(current_location);
             mMap.addMarker(options);
