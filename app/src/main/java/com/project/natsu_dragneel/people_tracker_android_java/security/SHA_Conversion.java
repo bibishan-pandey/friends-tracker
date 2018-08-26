86261ec71278e70e67a0d15d413b1fae71cd13f8
bivishanpandey <natsu.dragneel.bivishan@example.com>
2018-08-26 17:24:31 +0545

    SHA_Conversion inspected code commit

--- app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/security/SHA_Conversion.java
+++ app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/security/SHA_Conversion.java
@@ -5,11 +5,11 @@ import java.security.NoSuchAlgorithmException;
 
 public class SHA_Conversion {
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest md=MessageDigest.getInstance("SHA");
+        MessageDigest md = MessageDigest.getInstance("SHA");
         md.update(password.getBytes());
-        byte[] b=md.digest();
-        StringBuffer sb=new StringBuffer();
-        for(byte b1:b){
+        byte[] b = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (byte b1 : b) {
             sb.append(Integer.toHexString(b1 & 0xff));
         }
         return sb.toString();
