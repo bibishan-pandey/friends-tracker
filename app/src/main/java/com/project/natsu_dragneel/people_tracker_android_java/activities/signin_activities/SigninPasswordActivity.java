9f53beec1943ff138cdeab7dbb790333f4f5af4b
bivishanpandey <natsu.dragneel.bivishan@example.com>
2018-08-26 17:33:33 +0545

    SigninPasswordActivity inspected code commit

--- app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/activities/signin_activities/SigninPasswordActivity.java
+++ app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/activities/signin_activities/SigninPasswordActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,10 +22,19 @@ import com.google.firebase.auth.FirebaseUser;
 import com.project.natsu_dragneel.people_tracker_android_java.MainActivity;
 import com.project.natsu_dragneel.people_tracker_android_java.R;
 import com.project.natsu_dragneel.people_tracker_android_java.activities.maps_activities.CurrentLocationActivity;
+import com.project.natsu_dragneel.people_tracker_android_java.security.SHA_Conversion;
+
+import java.security.NoSuchAlgorithmException;
 
 public class SigninPasswordActivity extends AppCompatActivity {
 
-    EditText signin_password_edittext;
+    private static final String TAG = SigninPasswordActivity.class.getSimpleName();
+
+    private static final String wait = "Please wait. Signing in.";
+    private static final String not_verified = "This email is not verified yet. Please check your email";
+    private static final String wrong_credentials = "Wrong username/password";
+
+    EditText signin_password_editText;
     Button signin_password_next_button;
     String signin_password_secure;
     FirebaseAuth auth;
@@ -39,46 +49,41 @@ public class SigninPasswordActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         dialog = new ProgressDialog(this);
 
-        signin_password_edittext = (EditText)findViewById(R.id.signup_profile_edittext);
+        signin_password_editText = findViewById(R.id.signup_profile_edittext);
 
-        /*
         try {
-            signin_password_secure= SHA_Conversion.hashPassword(signin_password_edittext.getText().toString());
+            signin_password_secure = SHA_Conversion.hashPassword(signin_password_editText.getText().toString());
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
-        }*/
+        }
 
-        signin_password_next_button = (Button)findViewById(R.id.signin_nav_click);
+        signin_password_next_button = findViewById(R.id.signin_nav_click);
 
         Intent intent = getIntent();
-        if (intent!=null)
-        {
+        if (intent != null) {
             email = intent.getStringExtra("email_login");
         }
 
         signin_password_next_button.setEnabled(false);
         signin_password_next_button.setBackgroundColor(Color.parseColor("#faebd7"));
 
-        signin_password_edittext.addTextChangedListener(new TextWatcher() {
+        signin_password_editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+                Log.d(TAG, "beforeTextChanged: beforeTextChanged");
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                Log.d(TAG, "onTextChanged: onTextChanged");
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()>=6)
-                {
+                if (s.length() >= 6) {
                     signin_password_next_button.setEnabled(true);
                     signin_password_next_button.setBackgroundColor(Color.parseColor("#f05545"));
-                }
-                else
-                {
+                } else {
                     signin_password_next_button.setEnabled(false);
                     signin_password_next_button.setBackgroundColor(Color.parseColor("#faebd7"));
                 }
@@ -86,13 +91,11 @@ public class SigninPasswordActivity extends AppCompatActivity {
         });
     }
 
-    public void Login(View v)
-    {
-        dialog.setMessage("Please wait. Signing in.");
+    public void Login(View v) {
+        dialog.setMessage(wait);
         dialog.show();
-        if(signin_password_edittext.getText().toString().length()>=6)
-        {
-            auth.signInWithEmailAndPassword(email,signin_password_edittext.getText().toString())
+        if (signin_password_editText.getText().toString().length() >= 6) {
+            auth.signInWithEmailAndPassword(email, signin_password_secure)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -107,30 +110,29 @@ public class SigninPasswordActivity extends AppCompatActivity {
                                     dialog.dismiss();
                                     finish();
                                     FirebaseAuth.getInstance().signOut();
-                                    Toast.makeText(getApplicationContext(), "This email is not verified yet. Please check your email", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), not_verified, Toast.LENGTH_SHORT).show();
                                     Intent myIntent = new Intent(SigninPasswordActivity.this, MainActivity.class);
                                     startActivity(myIntent);
                                 }
                             } else {
                                 dialog.dismiss();
-                                Toast.makeText(getApplicationContext(), "Wrong username/password", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), wrong_credentials, Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-
         }
     }
 
     @Override
     public void onBackPressed() {
         finish();
-        Intent intent=new Intent(SigninPasswordActivity.this, MainActivity.class);
+        Intent intent = new Intent(SigninPasswordActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void back_image_button(View v){
+    public void back_image_button(View v) {
         finish();
-        Intent intent=new Intent(SigninPasswordActivity.this, MainActivity.class);
+        Intent intent = new Intent(SigninPasswordActivity.this, MainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/security/SHA_Conversion.java
+++ app/src/main/java/com/project/natsu_dragneel/people_tracker_android_java/security/SHA_Conversion.java
@@ -3,6 +3,7 @@ package com.project.natsu_dragneel.people_tracker_android_java.security;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@SuppressWarnings("StringBufferMayBeStringBuilder")
 public class SHA_Conversion {
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA");
